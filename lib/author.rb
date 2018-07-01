74c1aa7c2eefa4b0fb449eccbae001b720c8832f
louis baptiste <louisvis@ymail.com>
2018-07-01 08:10:09 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,7 +15,7 @@ end
 def add_post (post)
   post.author = self
   @posts << post
-  @@all_post << post 
+  @@all_post << post
 end
 
 
@@ -23,11 +23,11 @@ def add_post_by_title(post_title)
   post = Post.new(post_title)
   post.author = self
   @posts << post
-  @@all_post << post 
+  @@all_post << post
 end
 
 def self.post_count
-   @@all_post.count 
+   @@all_post.count
 end
 
 end
