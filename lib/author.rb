e9f942886b4bb4b57e3e263aeee015884cfd212c
louis baptiste <louisvis@ymail.com>
2018-07-01 08:10:01 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
 attr_accessor :name , :posts
 
-
+@@all_post = []
 
 def initialize(name)
   @name = name
@@ -15,7 +15,7 @@ end
 def add_post (post)
   post.author = self
   @posts << post
-
+  @@all_post << post 
 end
 
 
@@ -23,7 +23,11 @@ def add_post_by_title(post_title)
   post = Post.new(post_title)
   post.author = self
   @posts << post
+  @@all_post << post 
 end
 
+def self.post_count
+   @@all_post.count 
+end
 
 end
